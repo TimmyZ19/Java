bbd8fd018ea0937b6799e26d21ef5e8a3b29378e
TimmyZ <zalamar1704@gmail.com>
2022-11-28 13:21:05 +0500

    Семинар 1. Задача 1

---
+++ Sem1_HW/src/Zadanie1.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Zadanie1 {
+    public static void main(String[] args) {
+        Scanner iScanner = new Scanner(System.in);
+        System.out.print("Введите число n: ");
+        int number = iScanner.nextInt();
+        iScanner.close();
+        int sum = 0;
+        for (int i = 1; i <= number; i++) {
+            sum += i;
+        }
+        int comp = 1;
+        for (int i = 1; i <= number; i++) {
+            comp *= i;
+        }
+        System.out.printf("Сумма чисел от 1 до %d -> %d", number, sum);
+        System.out.println();
+        System.out.printf("Произведение чисел от 1 до %d -> %d", number, comp);
+    }
+}
