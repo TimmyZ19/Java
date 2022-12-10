d4fdf3178ddff361f113298cbaf77eade2af113d
TimmyZ <zalamar1704@gmail.com>
2022-12-10 13:02:20 +0500

    Задача 1. Семинар 5

---
+++ Sem5_HW/src/ZD1.java
@@ -0,0 +1,74 @@
+/*
+Реализуйте структуру телефонной книги с помощью HashMap, учитывая, что 1 человек может иметь несколько телефонов.
+ */
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class ZD1 {
+    public static void phonebook(HashMap<Integer, String> map) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            choice:
+            while (true) {
+                System.out.print(
+                        "Введите:\n1 - для поиска контакта\n2 - для добавления контакта\n" +
+                                "3 - для удаления контакта\n4 - для просмотра всей телефонной книги\n" +
+                                "q - для выхода \n");
+                String input = scanner.nextLine();
+                switch (input) {
+                    case ("1"):
+                        System.out.println("Введите фамилию контакта");
+                        String name = scanner.nextLine();
+                        for (var item : map.entrySet())
+                            if (item.getValue().equals(name)) {
+                                System.out.println("Контакт " + name + " найден: " + item.getKey());
+                            }
+
+                        break;
+
+                    case ("2"):
+                        System.out.println("Введите фамилию: ");
+                        name = scanner.nextLine();
+                        System.out.println("Введите номер телефона: ");
+                        int key = scanner.nextInt();
+                        map.putIfAbsent(key, name);
+                        System.out.println(map);
+                        break;
+                    case ("3"):
+                        System.out
+                                .println("Введите номер телефона контакта телефонной книги, который хотите удалить: ");
+                        key = scanner.nextInt();
+                        ;
+                        map.remove(key);
+                        System.out.println(map);
+                        break;
+                    case ("4"):
+                        System.out.println(map);
+                        break;
+
+                    case ("q"):
+                        break choice;
+                }
+            }
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+
+        HashMap<Integer, String> phonebook = new HashMap<>();
+
+        phonebook.put(89628886, "Ivanov");
+        phonebook.put(865235235, "Sidoroff");
+        phonebook.put(80833222, "Suslov");
+        phonebook.put(1811222, "Petrov");
+        phonebook.put(1634535, "Ivanov");
+        phonebook.put(867475, "Ivanov");
+
+        System.out.println(phonebook);
+        phonebook(phonebook);
+
+    }
+
+}
