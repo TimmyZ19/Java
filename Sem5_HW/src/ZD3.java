d182034f1145132bbe2ebc38c07cd597b82e02a0
TimmyZ <zalamar1704@gmail.com>
2022-12-10 13:26:26 +0500

    Задача 3. Семинар 5

---
+++ Sem5_HW/src/ZD3.java
@@ -0,0 +1,54 @@
+/*
+ Реализовать алгоритм пирамидальной сортировки (HeapSort)
+ */
+
+
+public class ZD3 {
+    public void sort(int arr[]) {
+        int N = arr.length;
+
+        for (int i = N / 2 - 1; i >= 0; i--)
+            heapify(arr, N, i);
+
+        for (int i = N - 1; i > 0; i--) {
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+            heapify(arr, i, 0);
+        }
+    }
+
+    void heapify(int arr[], int N, int i) {
+        int largest = i;
+        int l = 2 * i + 1;
+        int r = 2 * i + 2;
+
+        if (l < N && arr[l] > arr[largest])
+            largest = l;
+
+        if (r < N && arr[r] > arr[largest])
+            largest = r;
+
+        if (largest != i) {
+            int swap = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = swap;
+            heapify(arr, N, largest);
+        }
+    }
+
+    static void printArray(int arr[]) {
+        int N = arr.length;
+
+        for (int i = 0; i < N; ++i)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+
+    public static void main(String args[]) {
+        int arr[] = {2, 7, 21, 16, 8, 25};
+        ZD3 ob = new ZD3();
+        ob.sort(arr);
+        printArray(arr);
+    }
+}
